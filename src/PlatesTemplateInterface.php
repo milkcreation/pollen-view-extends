1e77bc857cb13d918826c1c541cdfd7875fa3e0c
Jordy Manner <jordy.manner@milkcreation.fr>
2021-06-24 16:43:25 +0000

    view-extends >> master

---
+++ src/PlatesTemplateInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\ViewExtends;
+
+use Pollen\View\Engines\Plates\PlatesTemplateInterface as BasePlatesTemplateInterface;
+
+/**
+ * @method string field(string $alias, string|array $idOrParams = null, array|null $params = null)
+ * @method string partial(string $alias, string|array $idOrParams = null, array|null $params = null)
+ */
+interface PlatesTemplateInterface extends BasePlatesTemplateInterface
+{
+}
